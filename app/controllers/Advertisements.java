742eba27483bb81f6e7b55d31252ca6b00145078
balancy <balancy@localhost>
2014-08-25 15:24:43 +0600

    added controllers routes change

---
+++ app/controllers/Advertisements.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class Advertisements extends Controller {
+
+	public static Result add(){
+		return TODO;
+	}
+	
+	public static Result extend(Long id){
+		return TODO;
+	}
+	
+	public static Result delete(Long id){
+		return TODO;
+	}
+}
---
+++ app/controllers/Filters.java
@@ -0,0 +1,48 @@
+package controllers;
+
+import models.ad.Breed;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class Filters extends Controller {
+
+	public static Result addBreed(){
+		return TODO;
+	}
+	
+	public static Result updateBreed(Long id){
+		return TODO;
+	}
+	
+	public static Result deleteBreed(Long id){
+//		Breed
+		return TODO;
+	}
+	
+	public static Result addRegion(){
+		return TODO;
+	}
+	
+	public static Result updateRegion(Long id){
+		return TODO;
+	}
+	
+	public static Result deleteRegion(Long id){
+//		Breed
+		return TODO;
+	}
+	
+	public static Result addCity(){
+		return TODO;
+	}
+	
+	public static Result updateCity(Long id){
+		return TODO;
+	}
+	
+	public static Result deleteCity(Long id){
+//		Breed
+		return TODO;
+	}	
+	
+}
