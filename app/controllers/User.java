20a6b7b49c757340f581c2a2f39e4dbfe3a9fa91
askhat_aitimov <askhat_aitimov@localhost>
2014-08-26 12:51:19 +0600

    ajax added but with mistake

---
+++ app/controllers/User.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import static play.data.Form.form;
+import controllers.routes;
+import play.data.DynamicForm;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.admin.ads.*;
+import views.html.admin.filters.*;
+import views.html.admin.advertisements.*;
+import views.html.admin.users.*;
+
+public class User extends Controller {
+	
+	
+	public static Result register(){
+		
+		DynamicForm requestData = form().bindFromRequest();
+		
+		System.out.println(requestData);
+		return ok("baaa");
+		//return redirect(routes.Manage.myInfo());
+	}
+	
+}
---
+++ app/models/ad/Quantity.java
@@ -0,0 +1,25 @@
+package models.ad;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import play.db.ebean.Model;
+
+@Entity
+public class Quantity extends Model {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	public Long id;
+	
+	public String name;
+	
+	@OneToMany
+	public Ad ad;
+	
+}
