cb0008a23e25b1a4539b0a2617944452bafd1175
balancy <balancy@localhost>
2014-08-26 11:47:32 +0600

    my removed

--- app/controllers/Info.java
+++ app/controllers/Info.java
@@ -2,7 +2,7 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.profile.myInfo.edit.*;
+import views.html.profile.info.edit.*;
 public class Info extends Controller {
 	
 	public static Result edit(){
