e9ecff31d645f83f21db05e5f559d088af9fac8d
balancy <balancy@localhost>
2014-08-27 19:30:24 +0600

    registration refactor

--- app/controllers/User.java
+++ app/controllers/User.java
@@ -5,6 +5,7 @@ import controllers.routes;
 import models.user.AuthorisedUser;
 import models.user.SecurityRole;
 import play.data.DynamicForm;
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.admin.ads.*;
@@ -15,36 +16,21 @@ import views.html.admin.users.*;
 import java.util.ArrayList;
 
 public class User extends Controller {
-	
-	
+
 	public static Result register() {
-
-        DynamicForm requestData = form().bindFromRequest();
-
-        System.out.println(requestData);
-
-		if(AuthorisedUser.find.where().eq("email",requestData.get("email")).findUnique()==null){
-
-            AuthorisedUser user = new AuthorisedUser();
-            user.email = requestData.get("email");
-            user.password = requestData.get("password");
-            user.userName = requestData.get("nameOfUserOrCompany");
-            SecurityRole role = SecurityRole.findByName("user");
-            user.roles = new ArrayList<SecurityRole>();
-            user.roles.add(role);
-            user.save();
-            return ok("success");
-        }else
-        {
-
-            return ok("error");
-
-        }
-
-		//return redirect(routes.Manage.myInfo());
+		Form<AuthorisedUser> userForm = form(AuthorisedUser.class).bindFromRequest();
+		if(userForm.hasErrors()){
+			return ok();
+		}
+		else if (AuthorisedUser.find.where().eq("email", userForm.get().email).findUnique() == null) {
+			AuthorisedUser user = userForm.get();
+			user.roles = new ArrayList<SecurityRole>();
+			user.roles.add(SecurityRole.findByName("user"));
+			user.save();
+			return ok("success");
+		} else {
+			return ok("error");
+		}
+		// return redirect(routes.Manage.myInfo());
 	}
-
-//        return ok("gb");
-//    }
-
 }
