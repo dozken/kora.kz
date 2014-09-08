b90a89b27e7ec1aba031612bd353649561852ecc
askhat_aitimov <askhat_aitimov@localhost>
2014-09-08 11:39:35 +0600

    create ads in process

--- app/controllers/Ads.java
+++ app/controllers/Ads.java
@@ -1,10 +1,11 @@
 package controllers;
 
 import models.ad.Ad;
+import models.ad.Animal;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.ad.create.createAd;
+import views.html.ad.create.*;
 import views.html.ad.search.adSearch;
 import views.html.ad.show.showAd;
 
@@ -18,18 +19,24 @@ public class Ads extends Controller {
 	}
 
 	public static Result create() {
-		Ad ad = new Ad();
-		ObjectNode event = Json.newObject();
-		event.put("pending","active");	
-		ad.status = "pending";
-		ad.update();
-		event.put("id",ad.id);
-		event.put("title",ad.title);
-		event.put("company",ad.contactInfo.company);
-		event.put("moderatingBy",models.user.AuthorisedUser.findByEmail(session("connected")).userName);
-		WS.send(event);
+//		Ad ad = new Ad();
+//		ObjectNode event = Json.newObject();
+//		event.put("pending","active");
+//		ad.status = "pending";
+//		//ad.update();
+//		event.put("id",ad.id);
+//		event.put("title",ad.title);
+//		event.put("company",ad.contactInfo.company);
+//		event.put("moderatingBy",models.user.AuthorisedUser.findByEmail(session("connected")).userName);
+//		WS.send(event);
+
+
 		return ok(createAd.render());
 	}
+
+    public static Result getBreeds(Long id){ System.out.println(id); return ok(_breed.render(Animal.find.byId(id)));}
+
+    public static Result show(){ return ok(createAd.render()); }
 	
 	public static Result search() {
 		return ok(adSearch.render());
