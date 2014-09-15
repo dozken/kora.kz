e46010166db241afe677773efa61c85af34cd097
askhat_aitimov <askhat_aitimov@localhost>
2014-09-15 12:48:31 +0600

    validation+

--- public/javascripts/validation.js
+++ public/javascripts/validation.js
@@ -81,3 +81,82 @@ function validation(){
 
 	return r;
 }
+
+function register_validation(){
+    var r=true;
+
+
+    $("input[Regvalidation]").each(function(){
+
+
+        $(this).removeAttr("style");
+        var type = $(this).attr("Regvalidation");
+
+        if(type=="not_null"){
+            var a=$(this).val();
+            if(a==null || a==""){
+                r=false;
+                $(this).attr("style","border-color:red");
+            }
+        }
+
+        if(type=="mail"){
+            var a=$(this).val();
+            var re = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+            if(!a.match(re)){
+                r=false;
+                $(this).attr("style","border-color:red");
+
+            }
+        }
+    });
+
+    if(!r){
+        return r;
+    }
+
+    if($('[name="password"]').val() != $('[name="confirmPassword"]').val()){
+
+        alert("Paswords not same!!!");
+        return false;
+    }
+
+    if (!$('[name="agreement"]').is(':checked')){
+        alert("please check agreement to register");
+        r=false;
+    }
+
+    return r;
+}
+
+function login_validation(){
+    var r=true;
+
+
+    $("input[Logvalidation]").each(function(){
+
+
+        $(this).removeAttr("style");
+        var type = $(this).attr("Logvalidation");
+
+        if(type=="not_null"){
+            var a=$(this).val();
+            if(a==null || a==""){
+                r=false;
+                $(this).attr("style","border-color:red");
+            }
+        }
+
+        if(type=="mail"){
+            var a=$(this).val();
+            var re = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+            if(!a.match(re)){
+                r=false;
+                $(this).attr("style","border-color:red");
+
+            }
+        }
+    });
+
+    return r;
+}
