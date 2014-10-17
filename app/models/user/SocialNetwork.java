8bad4a4afe8d211a9b672655e1b6e1cf11c9d2a2
Aitimov <Aitimov@localhost>
2014-10-17 11:15:41 +0600

    map call when need

---
+++ app/models/user/SocialNetwork.java
@@ -0,0 +1,24 @@
+package models.user;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import play.db.ebean.Model;
+
+@Entity
+public class SocialNetwork extends Model {
+
+    public static final Model.Finder<Long, SocialNetwork> find = new Model.Finder<Long, SocialNetwork>(
+            Long.class, SocialNetwork.class);
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	public Long id;
+	
+	public String name;
+
+}
