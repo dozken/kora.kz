061aae66c5ed98148742015597f10d895c70e96d
aslanbek_zholdygarayev <aslanbek_zholdygarayev@localhost>
2014-11-11 17:59:07 +0600

    tipo polnyi css

---
+++ app/models/ad/AdSetting.java
@@ -0,0 +1,39 @@
+package models.ad;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import play.db.ebean.Model;
+
+@Entity
+public class AdSetting extends Model {
+
+	/**
+	 * 
+	 */
+	
+	public static final Model.Finder<Long, AdSetting> find = new Model.Finder<Long, AdSetting>(
+			Long.class, AdSetting.class);
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	public Long id;
+	
+	@ManyToOne
+	public Ad ad;
+	/**
+	 * TODO
+	 * 1. only registered can comment
+	 * 2. auto extend expiration date of ad
+	 * 3. allocate ad(vydelit')
+	 */
+	
+	public String name;
+	
+	public String status;
+	
+	public Date validDate =new Date(new Date().getTime() + (1000 * 60 * 60 * 24 * 7));
+}
---
+++ app/models/contact/City.java
@@ -0,0 +1,43 @@
+package models.contact;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+import models.Location;
+import models.user.Profile;
+import play.db.ebean.Model;
+
+@Entity
+public class City extends Model {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public static final Model.Finder<Long, City> find = new Model.Finder<Long, City>(
+			Long.class, City.class);
+	@Id
+	public Long id;
+	
+	@ManyToOne
+	public Region region;
+
+	public String name;
+	
+	@OneToOne(cascade=CascadeType.ALL)
+	public Location location;
+	
+	@OneToMany(mappedBy="city")
+	public List<Profile> profiles;
+	
+	@OneToMany(mappedBy="city")	
+	public List<ContactInfo> contactInfos;
+
+}
