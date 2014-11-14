3cc2d158223ed991c783eba5dc9fc521789a39c4
balancy <balancy@localhost>
2014-11-14 15:32:15 +0600

    sitemap added, aniamtion to index items

---
+++ app/models/user/UserSetting.java
@@ -0,0 +1,31 @@
+package models.user;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import models.Setting;
+import play.db.ebean.Model;
+
+@Entity
+public class UserSetting extends Model {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public static final Model.Finder<Long, UserSetting> find = new Model.Finder<Long, UserSetting>(
+			Long.class, UserSetting.class);
+
+	@Id
+	public Long id;
+
+	@ManyToOne
+	public AuthorisedUser user;
+
+	@ManyToOne
+	public Setting setting;
+
+	public String status;
+}
