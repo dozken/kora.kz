c271f5896e4ff793fc625581709d08b018ae60e1
aslanbek_zholdygarayev <aslanbek_zholdygarayev@localhost>
2014-11-19 14:05:35 +0600

    1

---
+++ app/controllers/Administer.java
@@ -0,0 +1,74 @@
+package controllers;
+
+import static play.data.Form.form;
+import models.Emailing;
+import models.ad.Ad;
+import play.data.DynamicForm;
+import play.libs.Json;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.admin.ads.adminAds;
+import views.html.admin.advertisements.adminAdvertisements;
+import views.html.admin.filters.adminFilters;
+import views.html.admin.moderators.adminModerators;
+import views.html.admin.users.adminUsers;
+import views.html.mailBody.recoverPassword;
+import views.html.mailBody.adModerate;
+import be.objectify.deadbolt.java.actions.Group;
+import be.objectify.deadbolt.java.actions.Restrict;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+@Restrict(@Group("admin"))
+public class Administer extends Controller {
+
+	@Restrict({ @Group("moderator"), @Group("admin") })
+	public static Result ads() {
+		return ok(adminAds.render());
+	}
+
+	public static Result filters() {
+		return ok(adminFilters.render());
+	}
+
+	public static Result advetiments() {
+		return ok(adminAdvertisements.render());
+	}
+
+	public static Result users() {
+		return ok(adminUsers.render());
+	}
+
+	public static Result moderators() {
+		return ok(adminModerators.render());
+	}
+
+	@Restrict({ @Group("admin"), @Group("moderator") })
+	public static Result moderate(Long id) {
+		Ad ad = Ad.find.byId(id);
+		DynamicForm requestData = form().bindFromRequest();
+		ad.status = requestData.get("status");
+		ad.update();
+
+		/**
+		 * moderating active rejected
+		 */
+		if(ad.status.equals("active")||ad.status.equals("rejected"))
+		Emailing.send("Қора.kz: Ваше объявление",
+				new String[] { ad.contactInfo.company + " <"
+						+ ad.contactInfo.email + ">" },
+				adModerate.render(ad).body());
+		if (requestData.get("status").equals("moderating")) {
+			ObjectNode event = Json.newObject();
+			event.put("moderating", "active");
+			event.put("id", ad.id);
+			event.put("title", ad.title);
+			event.put("company", ad.contactInfo.company);
+			event.put("moderatingBy", models.user.AuthorisedUser
+					.findByEmail(session("connected")).userName);
+			WS.send(event);
+		}
+		// return ok();
+		return redirect(routes.Ads.get(id));
+	}
+}
---
+++ app/models/ad/AdImage.java
@@ -0,0 +1,49 @@
+package models.ad;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import play.db.ebean.Model;
+
+@Entity
+public class AdImage extends Model {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public static final Finder<Long, AdImage> find = new Finder<Long, AdImage>(
+			Long.class, AdImage.class);
+
+	@ManyToOne
+	public Ad ad;
+	
+	@Id
+	public Long id;
+	
+    public String name;
+
+    @Column(columnDefinition = "TEXT")
+	public String content;
+
+
+    public Integer position;
+
+	public static String getFirstPicture(Long id){
+
+        return AdImage.find.where().eq("ad_id",id).eq("position",1).findUnique().content;
+
+    }
+
+    public static List<AdImage> getPictureByPosition(Long id){
+
+        return AdImage.find.where().eq("ad_id",id).order("position").findList();
+
+    }
+
+}
