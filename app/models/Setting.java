ffcfcd271dbffb9b64665338fa7d5319691fc4b8
aslanbek_zholdygarayev <aslanbek_zholdygarayev@localhost>
2014-12-03 16:36:54 +0600

    ???????? ?????? ??????????

---
+++ app/models/Setting.java
@@ -0,0 +1,44 @@
+package models;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import models.admin.AdminSetting;
+import models.user.UserSetting;
+import play.db.ebean.Model;
+
+@Entity
+public class Setting extends Model {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public static final Model.Finder<Long, Setting> find = new Model.Finder<Long, Setting>(
+			Long.class, Setting.class);
+	
+	@Id
+	public Long id;
+	
+	public Integer position;
+	
+	public String name;
+	
+	public String category;
+	
+	@OneToMany
+	public List<UserSetting> userSettings;
+	
+	@OneToMany
+	public List<AdminSetting> adminSettings;
+	
+	public Double price;
+	
+	public static List<Setting> findByCategory(String category){
+		return find.where().eq("category", category).orderBy("position").findList();		
+	}
+}
---
+++ app/models/admin/AdminSetting.java
@@ -0,0 +1,33 @@
+package models.admin;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import models.Setting;
+import models.user.AuthorisedUser;
+import play.db.ebean.Model;
+
+@Entity
+public class AdminSetting extends Model {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public static final Model.Finder<Long, AdminSetting> find = new Model.Finder<Long, AdminSetting>(
+			Long.class, AdminSetting.class);
+	
+	@Id
+	public Long id;
+	
+	@ManyToOne
+	public AuthorisedUser user;
+	
+	@ManyToOne
+	public Setting setting;
+
+	public String status;
+	
+}
