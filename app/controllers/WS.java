12de99181735badf4c3fced4f1e617b88c8432bb
balancy <balancy@localhost>
2014-12-09 09:26:11 +0600

    approve deny shown only in moderating status of ad

---
+++ app/controllers/WS.java
@@ -0,0 +1,50 @@
+package controllers;
+
+import java.util.ArrayList;
+
+import play.libs.F.Callback;
+import play.libs.F.Callback0;
+import play.libs.Json;
+import play.mvc.WebSocket;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+public class WS {
+
+	public static ArrayList<WebSocket.Out<JsonNode>> channels = new ArrayList<WebSocket.Out<JsonNode>>();
+
+	public static WebSocket<JsonNode> socket = new WebSocket<JsonNode>() {
+
+		// called when the websocket is established
+		public void onReady(WebSocket.In<JsonNode> in,
+				WebSocket.Out<JsonNode> out) {
+
+			channels.add(out);
+
+			// When the message come.
+			in.onMessage(new Callback<JsonNode>() {
+				public void invoke(JsonNode event) {
+				}
+			});
+
+			// When the socket is closed.
+			in.onClose(new Callback0() {
+				public void invoke() {
+					ObjectNode event = Json.newObject();
+					event.put("connection", "disconnected");
+				}
+			});
+
+			ObjectNode event = Json.newObject();
+			event.put("connection", "estabilished");
+			out.write(event);
+		}
+	};
+
+	public static void send(JsonNode event) {
+		for (WebSocket.Out<JsonNode> channel : channels) {
+			channel.write(event);
+		}
+	}
+}
