4fdd12343cc45c1de85c2de8cc99a1cace2c882e
balancy <balancy@localhost>
2014-12-09 16:46:15 +0600

    parent city view fixed in all other views

---
+++ app/models/ad/Breed.java
@@ -0,0 +1,33 @@
+package models.ad;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import play.db.ebean.Model;
+
+@Entity
+public class Breed extends Model {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public static final Model.Finder<Long, Breed> find = new Model.Finder<Long, Breed>(
+			Long.class, Breed.class);
+	@Id
+	public Long id;
+
+	@ManyToOne
+	public Animal animal;
+
+	public String name;
+
+	@OneToMany
+	public List<Ad> ads;
+
+}
