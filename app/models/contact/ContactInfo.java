430d15216e479d5a0aeb03d09016faabab9e0129
balancy <balancy@localhost>
2014-12-12 16:40:55 +0600

    sub category added, code format, optimize

---
+++ app/models/contact/ContactInfo.java
@@ -0,0 +1,97 @@
+package models.contact;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import models.Location;
+import models.ad.Ad;
+import models.user.AuthorisedUser;
+import models.user.Profile;
+import play.db.ebean.Model;
+
+@Entity
+@Table(name = "contacts")
+public class ContactInfo extends Model {
+
+	/**
+	 * 
+	 */
+	public static final Model.Finder<Long, ContactInfo> find = new Model.Finder<Long, ContactInfo>(
+			Long.class, ContactInfo.class);
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	public Long id;
+
+	@OneToOne(mappedBy = "contactInfo")
+	public Ad ad;
+
+	@ManyToOne
+	public Region region;
+
+	@ManyToOne
+	public City city;
+
+	public String company;
+
+	public String phone;
+
+	public String email;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	public Location location;
+
+	public ContactInfo() {
+
+	}
+
+	public ContactInfo(Region r, String c, String p, String e, String m) {
+		region = r;
+		company = c;
+		phone = "";
+
+		email = e;
+
+		save();
+
+	}
+
+	public static String phoneCheck(AuthorisedUser user, String phone) {
+
+		phone = phone.replaceAll("[-+ )(]", "");
+
+		if (user.profile.phone != null && user.profile.phone.equals(phone)) {
+			return phone;
+		} else {
+			int row = Profile.find.where().eq("phone", phone).findRowCount();
+
+			if (row > 0) {
+				return "exists";
+			}
+		}
+		return phone;
+	}
+
+	public static String phoneCorrect(String phone) {
+
+		return phone.replaceAll("[-+ )(]", "");
+	}
+
+	public static String splitPhone(String phone) {
+
+		String res = "";
+		String[] s = phone.split(",");
+		for (String tel : s) {
+			if (tel != null && !tel.equals("")) {
+				tel = tel.replaceAll("[-+ )(]", "");
+				res += "," + tel;
+
+			}
+		}
+		return res;
+	}
+}
---
+++ app/models/user/UserSocials.java
@@ -0,0 +1,67 @@
+package models.user;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import play.db.ebean.Model;
+
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.SqlQuery;
+import com.avaje.ebean.SqlRow;
+
+@Entity
+@Table(name = "user_socials")
+public class UserSocials extends Model {
+
+	private static final long serialVersionUID = 1L;
+
+	public static final Model.Finder<Long, UserSocials> find = new Model.Finder<Long, UserSocials>(
+			Long.class, UserSocials.class);
+
+	@Id
+	public Long id;
+
+	@ManyToOne
+	public AuthorisedUser user;
+
+	@ManyToOne
+	public SocialNetwork socialNetwork;
+
+	public String value;
+
+	public static UserSocials fillSocilal(String name, String value) {
+		UserSocials socials = new UserSocials();
+		socials.socialNetwork = SocialNetwork.find.where().eq("name", name)
+				.findUnique();
+		socials.value = value;
+		return socials;
+
+	}
+
+	public static String getSocialByName(Long id, String name) {
+		String sql = "select value from socials_user where user_id="
+				+ id.toString()
+				+ " and social_network_id in (select id from social_network where name='"
+				+ name + "')";
+		SqlQuery sqlQuery = Ebean.createSqlQuery(sql);
+		List<SqlRow> list = sqlQuery.findList();
+		if (list.size() > 0) {
+			return myTrim(list.get(0).values().toString());
+		}
+		return "";
+	}
+
+	public static String myTrim(String a) {
+		String b = "";
+		for (int i = 1; i < a.length() - 1; i++) {
+			b += a.charAt(i);
+
+		}
+		return b;
+	}
+
+}
