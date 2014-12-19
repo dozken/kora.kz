d0884eb6494f40b5e60c8981287939995dc3b7da
balancy <balancy@localhost>
2014-12-19 10:52:15 +0600

    world wrap favaorite font change

---
+++ app/models/ad/Image.java
@@ -0,0 +1,37 @@
+package models.ad;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.apache.commons.codec.binary.Base64;
+
+import play.data.validation.Constraints;
+import play.db.ebean.Model;
+
+@Entity
+@Table(name = "images")
+public class Image extends Model {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public static final Finder<Long, Image> find = new Finder<Long, Image>(
+			Long.class, Image.class);
+
+	@Id
+	public Long id;
+
+	public byte[] content;
+
+	@Constraints.Required
+	public String contentType;
+
+	// TODO
+
+	public static String byteToBase64(byte[] data) {
+		return Base64.encodeBase64String(data);
+	}
+}
